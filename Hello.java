de58f194a2ae61feb143bd3c0c661ab4089ec743
singhg24 <54372458+singhg24@users.noreply.github.com>
2019-08-21 13:47:29 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+	public static void printHeading(){
+		System.out.println("Gurumanpreet Singh");
+		System.out.println("Hello Java Program");
+		System.out.println("Lab 1");
+		System.out.println("Version 1.0 8/21/19");
+		System.out.println("CMSC 255-002");
+	}
+	
+	public static void main(String[] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+		
